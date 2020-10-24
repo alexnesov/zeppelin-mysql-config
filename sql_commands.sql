4d32b05d6faae9209f38ce230f6d183bd833541e
alexnesov <alexandrenesovic@gmail.com>
2020-10-24 23:54:35 +0200

    quotes + NULL

--- sql_commands.sql
+++ sql_commands.sql
@@ -1,6 +1,5 @@
-create table financetest (no int(5), ticker varchar(10) primary key, company varchar(200), 
-sector varchar(90), industry varchar(250),country varchar(30), market_cap float(25) DEFAULT, p_e float(8) DEFAULT, 
-price float(8), change_ float(8), volume int(10));
+CREATE TABLE financetest (no int(5), ticker varchar(10) primary key, company varchar(200),
+sector varchar(90), industry varchar(250),country varchar(30), market_cap float(25) DEFAULT NULL, p_e float(8) DEFAULT NULL,price float(8), change_ float(8), volume int(10));
 
 LOAD DATA LOCAL INFILE 'Overview.csv'
 INTO TABLE financetest
@@ -8,4 +7,9 @@ COLUMNS TERMINATED BY ','
 OPTIONALLY ENCLOSED BY '"'
 ESCAPED BY '"'
 LINES TERMINATED BY '\n'
-IGNORE 1 LINES;
+IGNORE 1 LINES
+(no, ticker, company, sector, industry, country, @market_cap, @p_e, price, change_, volume)
+SET
+market_cap = NULLIF(@market_cap,''),
+p_e = NULLIF(@p_e,'')
+;
