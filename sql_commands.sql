6577b5061d35ef2b6a5f725813c2e5fa93ae98cd
alexnesov <alexandrenesovic@gmail.com>
2020-10-24 23:36:42 +0200

    quotes + NULL

--- sql_commands.sql
+++ sql_commands.sql
@@ -1,5 +1,5 @@
 create table financetest (no int(5), ticker varchar(10) primary key, company varchar(200), 
-sector varchar(90), industry varchar(250),country varchar(30), market_cap float(25), p_e float(8), 
+sector varchar(90), industry varchar(250),country varchar(30), market_cap float(25) DEFAULT, p_e float(8) DEFAULT, 
 price float(8), change_ float(8), volume int(10));
 
 LOAD DATA LOCAL INFILE 'Overview.csv'
